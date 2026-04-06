bdadb1494a745c63d8fadcad1701357db94b90b8
prathoseraaj <prathoseraaj0312@gmail.com>
2026-04-06 15:19:20 +0530

    feat: main function

--- main.py
+++ main.py
@@ -2,3 +2,23 @@ import analyzer
 from fastapi import FastAPI
 from analyzer.parser import parse_code
 from analyzer.llm_extractor import extract_logic
+
+app = FastAPI()
+
+@app.post("/analyze")
+async def analyze_code(code:str):
+    ast_insight = parse_code(code)
+
+    if "error" in ast_insight:
+        return {"error": ast_insight["error"]}
+
+    results = extract_logic(code, ast_insight)
+
+    return {
+        "functions": ast_insight["functions"],
+        "loops": ast_insight["loops_count"],
+        "time_complexity": results.time_complexity if results else None,
+        "space_complexity": results.space_complexity if results else None,
+        "timeline_steps": len(results.logic_timeline) if results else 0
+    }
+
